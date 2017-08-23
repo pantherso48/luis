098b2af50840d337fb0d08a28ce99add3a4eb62e
reid <rcarpe03@gmail.com>
2017-08-23 13:00:15 -0400

    init

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ require('dotenv-extended').load();
 
 var restify = require('restify');
 var builder = require('botbuilder');
+var calling = require('botbuilder-calling');
 var fs = require('fs');
-const path = require('path');
+var path = require('path');
 var util = require('util');
+var prompts = require('./prompts');
 
 const publicPath = path.join(__dirname, '../luis/public');
+const model = process.env.LUIS_MODEL_URL;
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -23,10 +26,20 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-
-// Listen for messages from users
-server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+
+// Create calling bot
+var callConnector = new calling.CallConnector({
+    callbackUrl: process.env.CALLBACK_URL,
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var callBot = new calling.UniversalCallBot(callConnector);
+server.post('/api/calls', callConnector.listen());
+
+
 
 // var bot = new builder.UniversalBot(connector, [
 //     function (session) {
@@ -38,105 +51,139 @@ var bot = new builder.UniversalBot(connector);
 //     }
 // ]);
 
-const model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/cea9975e-843b-416e-b23f-4fa5c7fb823f?subscription-key=81ca0da9998c4ab6a023ea7348fdea61&timezoneOffset=0&verbose=true';
 const recognizer = new builder.LuisRecognizer(model);
 const intents = new builder.IntentDialog({ recognizers: [recognizer] });
 
-bot.dialog('/', intents);
+//=========================================================
+// Chat Dialogs
+//=========================================================
 
-intents.matches('getBilling', (session, results) => {
-  // session.userData.serverpath = 'http://26bb457d.ngrok.io';
-
-   session.say('Your Kronos code is MSABC001 and your business development code is BDXYZ123.',
-               'Your code is MSABC001 and your business development code is BDXYZ123.',
-                { inputHint: builder.InputHint.expectingInput });
-  // fs.readFile('./thought_leaders_in_ai.png', function (err, data) {
-  //     if (err) {
-  //         return session.send('Oops. Error reading file.');
-  //     }
-  //     if (results.entities[0].score >= .51) {
-  //       var contentType = 'image/png';
-  //       var base64 = Buffer.from(data).toString('base64');
-  //       var reply = new builder.Message(session)
-  //              .text('Your Kronos code is MSABC001 and your business development code is BDXYZ123.')
-  //              .addAttachment({ contentUrl: util.format('data:%s;base64,%s', contentType, base64),
-  //                               contentType: contentType,
-  //                               name: 'AI.png'
-  //                             });
-  //       session.send(reply);
-  //   }
-  //   console.log("Codes : "+results.entities[0].entity);
-  // })
-})
-.onDefault((session) => {
-    session.send('no intents matched');
+bot.dialog('/', function (session) {
+    session.send(prompts.chatGreeting);
 });
 
-intents.matches('getRequest', (session, results) => {
-  // session.userData.serverpath = 'http://26bb457d.ngrok.io';
+//=========================================================
+// Calling Dialogs
+//=========================================================
 
-   session.say('Derek, I’ve create a request for a new Chronos Code for Pinnacle Financial.',
-               'Derek, I’ve create a request for a new Chronos Code for Pinnacle Financial',
-                { inputHint: builder.InputHint.expectingInput });
-})
-.onDefault((session) => {
-    session.send('no intents matched');
-});
+callBot.dialog('/', [
+    function (session) {
+        // Send a greeting and start the menu.
+        if (!session.userData.welcomed) {
+            session.userData.welcomed = true;
+            session.send(prompts.welcome);
+            session.beginDialog('/demoMenu', { full: true });
+        } else {
+            session.send(prompts.welcomeBack);
+            session.beginDialog('/demoMenu', { full: false });
+        }
+    },
+    function (session, results) {
+        // Always say goodbye
+        session.send(prompts.goodbye);
+    }
+]);
 
-intents.matches('addPerson', (session, results) => {
-  // session.userData.serverpath = 'http://26bb457d.ngrok.io';
+bot.dialog('/demoMenu', [
+    function (session, args) {
+        // Build up a stack of prompts to play
+        var list = [];
+        list.push(calling.Prompt.text(session, prompts.demoMenu.prompt));
+        if (!args || args.full) {
+            list.push(calling.Prompt.text(session, prompts.demoMenu.choices));
+            list.push(calling.Prompt.text(session, prompts.demoMenu.help));
+        }
 
-   session.say('I have assigned Albert Smith to the Pinnacle project with Chronos code: PIN00123.',
-               'I have assigned Albert Smith to the Pinnacle project with Chronos code: PIN00123',
-                { inputHint: builder.InputHint.expectingInput });
-})
-.onDefault((session) => {
-    session.send('no intents matched');
-});
+        // Prompt user to select a menu option
+        calling.Prompts.choice(session, new calling.PlayPromptAction(session).prompts(list), [
+            { name: 'dtmf', speechVariation: ['dtmf'] },
+            { name: 'digits', speechVariation: ['digits'] },
+            { name: 'record', speechVariation: ['record', 'recordings'] },
+            { name: 'chat', speechVariation: ['chat', 'chat message'] },
+            { name: 'choices', speechVariation: ['choices', 'options', 'list'] },
+            { name: 'help', speechVariation: ['help', 'repeat'] },
+            { name: 'quit', speechVariation: ['quit', 'end call', 'hangup', 'goodbye'] }
+        ]);
+    },
+    function (session, results) {
+        if (results.response) {
+            switch (results.response.entity) {
+                case 'choices':
+                    session.send(prompts.demoMenu.choices);
+                    session.replaceDialog('/demoMenu', { full: false });
+                    break;
+                case 'help':
+                    session.replaceDialog('/demoMenu', { full: true });
+                    break;
+                case 'quit':
+                    session.endDialog();
+                    break;
+                default:
+                    // Start demo
+                    session.beginDialog('/' + results.response.entity);
+                    break;
+            }
+        } else {
+            // Exit the menu
+            session.endDialog(prompts.canceled);
+        }
+    },
+    function (session, results) {
+        // The menu runs a loop until the user chooses to (quit).
+        session.replaceDialog('/demoMenu', { full: false });
+    }
+]);
 
-
-
-
-
-// bot.dialog('findUserCodes', [
-//     function (session, args, next) {
-//         session.dialogData.profile = args || {}; // Set the profile or create the object.
-//         if (!session.dialogData.profile.name) {
-//             builder.Prompts.text(session, "What's your name?");
-//         } else {
-//             next(); // Skip if we already have this info.
-//         }
-//     },
-//     function (session, results, next) {
-//         if (results.response) {
-//             // Save user's name if we asked for it.
-//             session.dialogData.profile.name = results.response;
-//         }
-//         if (!session.dialogData.profile.company) {
-//             builder.Prompts.text(session, "What company do you work for?");
-//         } else {
-//             next(); // Skip if we already have this info.
-//         }
-//     },
-//     function (session, results) {
-//         if (results.response) {
-//             // Save company name if we asked for it.
-//             session.dialogData.profile.company = results.response;
-//         }
-//         session.endDialogWithResult({ response: session.dialogData.profile });
+// bot.dialog('/', intents);
+//
+// intents.matches('getBilling', (session, results) => {
+//
+// //note 1: SFB does not handle images
+//   //  session.say('Your Kronos code is MSABC001 and your business development code is BDXYZ123.',
+//   //              'Your code is MSABC001 and your business development code is BDXYZ123.',
+//   //               { inputHint: builder.InputHint.expectingInput });
+//   fs.readFile('./thought_leaders_in_ai.png', function (err, data) {
+//       if (err) {
+//           return session.send('Oops. Error reading file.');
+//       }
+//       if (results.entities[0].score >= .51) {
+//         var contentType = 'image/png';
+//         var base64 = Buffer.from(data).toString('base64');
+//         var reply = new builder.Message(session)
+//                .text('Your Kronos code is MSABC001 and your business development code is BDXYZ123.')
+//                .speak('Your Kronos code is MSABC001 and your business development code is BDXYZ123.', { inputHint: builder.InputHint.expectingInput })
+//                .addAttachment({ contentUrl: util.format('data:%s;base64,%s', contentType, base64),
+//                                 contentType: contentType,
+//                                 name: 'AI.png'
+//                               });
+//
+//         session.send(reply);
 //     }
-// ]);
-
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-// var bot = new builder.UniversalBot(connector);
-
-// bot.dialog('/', [
-//     // Step 1
-//     function (session) {
-//         builder.Prompts.text(session, 'Hi! What is your name?');
-//     },
-//     // Step 2
-//     function (session, results) {
-//         session.endDialog('Hello %s!', results.response);
-//     }
-// ]);
+//     console.log("Codes : "+results.entities[0].entity);
+//   })
+// })
+// .onDefault((session) => {
+//     session.send('no intents matched');
+// });
+//
+// intents.matches('getRequest', (session, results) => {
+//   // session.userData.serverpath = 'http://26bb457d.ngrok.io';
+//
+//    session.say('Derek, I’ve create a request for a new Chronos Code for Pinnacle Financial.',
+//                'Derek, I’ve create a request for a new Chronos Code for Pinnacle Financial',
+//                 { inputHint: builder.InputHint.expectingInput });
+// })
+// .onDefault((session) => {
+//     session.send('no intents matched');
+// });
+//
+// intents.matches('addPerson', (session, results) => {
+//   // session.userData.serverpath = 'http://26bb457d.ngrok.io';
+//
+//    session.say('I have assigned Albert Smith to the Pinnacle project with Chronos code: PIN00123.',
+//                'I have assigned Albert Smith to the Pinnacle project with Chronos code: PIN00123',
+//                 { inputHint: builder.InputHint.expectingInput });
+// })
+// .onDefault((session) => {
+//     session.send('no intents matched');
+// });
